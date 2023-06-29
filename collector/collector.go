531cd899b3442f234fd8c80f39f789b52d2c1bf7
Zzde <zhangxh1997@gmail.com>
2023-06-29 21:03:51 +0800

    Fix: increase connection collector conn read limit
    Signed-off-by: Zzde <zhangxh1997@gmail.com>

--- collector/collector.go
+++ collector/collector.go
@@ -29,15 +29,19 @@ func Start(config CollectConfig) {
 	for _, c := range collectors {
 		go func(c Collector) {
 			retryCount := 0
+			var err error
 			for retryCount < maxRetry {
-				if err := c.Collect(config); err != nil {
+				if err = c.Collect(config); err != nil {
 					retryCount++
 					sleepDuration := time.Duration(retryCount) * 10 * time.Second
-					log.Println("collector:", c.Name(), "failed, retry after", sleepDuration, "error: ", err)
+					log.Println("collector:", c.Name(), "failed error: ", err, "retry after", sleepDuration)
 					time.Sleep(sleepDuration)
 					continue
 				}
 				break
+			}
+			if err != nil {
+				log.Fatalf("error start collector %s, max retry exceeded will exit", c.Name())
 			}
 		}(c)
 	}
